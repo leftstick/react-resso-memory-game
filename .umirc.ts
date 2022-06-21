91c99b4349630647b0c88832c3688fc886fa6c59
Howard.Zuo <leftstick@qq.com>
2022-06-21 13:00:16 +0800

    chore: upgrade to new umi

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'umi'
 
 export default defineConfig({
+  npmClient: 'pnpm',
   hash: true,
   publicPath: '/react-resso-memory-game/',
   base: '/react-resso-memory-game/',
